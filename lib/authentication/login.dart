db3b15d50ac60ba480de5050a7dcb708accae3a9
Yoosin Paddy <yoosinfrancis@gmail.com>
2022-05-19 21:13:29 +0300

    startup

--- lib/authentication/login.dart
+++ lib/authentication/login.dart
@@ -1,4 +1,16 @@
 import 'package:flutter/material.dart';
 class Login extends StatefulWidget{
-  
+  @override
+  _LoginState createState()=>_LoginState();
+
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    // implement google facebook or mobile
+    return Scaffold(
+
+    );
+  }
 }
